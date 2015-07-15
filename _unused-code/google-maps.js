893983672bb8e08be35b13e7c7d5796fda9828d4
reyemtm <malcolmtmeyer@gmail.com>
2015-07-14 20:41:56 -0400

    footer

---
+++ _unused-code/google-maps.js
@@ -0,0 +1,2 @@
+	<!--<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRngKslUGJTlibkQ3FkfTxj3Xss1UlZDA&sensor=false"></script>
+	<!--<script src="{{ "/js/grayscalemap.js " | prepend: site.baseurl }}"></script>-->
