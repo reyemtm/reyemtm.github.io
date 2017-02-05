0cacfefc3a35d144132f83bb5aed777bab6b7de5
Malcolm <malcolm@localhost.localdomain>
2017-02-05 15:46:37 -0500

    test

--- README.md
+++
@@ -1,6 +0,0 @@
-##Clean Blog - Jekyll Version - Modified
-
-This is a modified version of the Clean Blog theme by [Start Bootstrap](http://startbootstrap.com/).
-
-##Added
- - Jekyll-Search Plugin
--- _unused-code/google-maps.js
+++
@@ -1,2 +0,0 @@
-	<!--<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRngKslUGJTlibkQ3FkfTxj3Xss1UlZDA&sensor=false"></script>
-	<!--<script src="{{ "/js/grayscalemap.js " | prepend: site.baseurl }}"></script>-->
